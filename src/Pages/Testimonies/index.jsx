68d5a8183da20fb48cce045708d9fbc010e49711
Tess <theresebazin2016@gmail.com>
2024-11-21 13:19:14 +0100

    changing google widget

--- src/Pages/Testimonies/index.jsx
+++ src/Pages/Testimonies/index.jsx
@@ -5,23 +5,30 @@ import { NavLink } from "react-router-dom";
 import { cards } from "./cardData";
 const Testimonies = () => {
   return (
-    <div className={styles.testimoniesContainer}>
-      <div className={styles.testimoniesTitle}>
-        <h2>Témoignages</h2>
-        <NavLink to="/Partage">
-          <button>Partager votre témoignage</button>
-        </NavLink>
+    <>
+      <div className={styles.testimoniesContainer}>
+        <div className={styles.testimoniesTitle}>
+          <h2>Témoignages</h2>
+          <NavLink to="/Partage">
+            <button>Partager votre témoignage</button>
+          </NavLink>
+        </div>
+        <div className={styles.testimonies}>
+          <ul>
+            {cards
+              .sort((a, b) => b.id - a.id)
+              .map((card) => (
+                <TestimonieCard key={card.id} card={card} />
+              ))}
+          </ul>
+        </div>
       </div>
-      <div className={styles.testimonies}>
-        <ul>
-          {cards
-            .sort((a, b) => b.id - a.id)
-            .map((card) => (
-              <TestimonieCard key={card.id} card={card} />
-            ))}
-        </ul>
-      </div>
-    </div>
+      <h2 className={styles.googleTitle}>Témoignages Google</h2>
+      <div
+        className={styles.googleTestimonies}
+        data-trustmary-widget="OBFfZmpgF"
+      ></div>
+    </>
   );
 };
 
